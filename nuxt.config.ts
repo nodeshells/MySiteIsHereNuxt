bc33885d9c78bfbb3edee61f8d4ad67c13ffd853
nodeshells <kenta.job09@gmail.com>
2023-06-10 23:46:58 +0900

    セキュリティ的に問題ないのでAPIキーをコードに埋め込んだ

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config';
 
-const config = {
-  public: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY || '',
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN || '',
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID || '',
-    FIREBASE_DATABASE_URL: process.env.FIREBASE_DATABASE_URL || '',
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET || '',
-    FIREBASE_MESSAGING_SENDER_ID:
-      process.env.FIREBASE_MESSAGING_SENDER_ID || '',
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID || '',
-    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID || '',
-  },
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyC3APkv0SSzXrNF7As3BqDtBfDzTzUcN4U',
+  authDomain: 'mysiteishere-nuxt.firebaseapp.com',
+  projectId: 'mysiteishere-nuxt',
+  storageBucket: 'mysiteishere-nuxt.appspot.com',
+  messagingSenderId: '575745537141',
+  appId: '1:575745537141:web:d2f874bb4578092653b43c',
+  measurementId: 'G-5CDBSV0B0W',
 };
 
 export default defineNuxtConfig({
   ssr: false,
-  runtimeConfig: config,
+  runtimeConfig: {
+    public: {
+      FIREBASE_CONFIG,
+    },
+  },
   app: {
     head: {
       title: 'MySite Is Here!',
